c1254be6b4d4c973ca624437a49dab5497210b69
Vicente <vicen.dark131@gmail.com>
2026-03-30 14:03:44 -0600

    Definiendo en el servidor la ruta de swwager

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import authRoutes from '@adapters/in/http/auth/auth.routes';
 import { errorMiddleware } from '@adapters/in/http/middlewares/error.middleware';
+import { swaggerSpec } from './swagger';
 
 // Configuración del servidor Express
 const app = express();
@@ -9,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Documentación Swagger
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Rutas
 app.use('/api/auth', authRoutes);
 
 // Middleware para manejo de errores
