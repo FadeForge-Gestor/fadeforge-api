587ffec1d6d546c4a8025fa8a7036254a577dd90
Vicente <vicen.dark131@gmail.com>
2026-03-31 08:50:39 -0600

    Declaramos da manera global a express

---
+++ src/shared/types/express.d.ts
@@ -0,0 +1,12 @@
+// Declaración de tipos para extender la interfaz Request de Express
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                id: number;
+                correo: string;
+                rol: number;
+            };
+        }
+    }
+}
