5d563afcac5c1ea293c0aa7d3ecbf17f8ecc29c9
Vicente <vicen.dark131@gmail.com>
2026-04-22 11:07:41 -0600

    Se creo el caso de uso para las credenciales y creando los cosas

--- src/core/usecases/credenciales/credenciales.usecase.ts
+++ src/core/usecases/credenciales/credenciales.usecase.ts
@@ -8,26 +8,40 @@ import { ConflictError, NotFoundError, UnauthorizedError } from "@shared/errors/
 export class CredencialUseCase implements ICredencialUseCase {
 
     // Inyección de dependencias del repositorio de credenciales
-    constructor(private readonly CredencialRepository: ICredencialRepository) {}
+    constructor(private readonly credencialRepository: ICredencialRepository) {}
 
     // Lógica del caso de uso para cambiar la contraseña
     async cambiarContrasena(idUsuario: number, input: CambiarContrasenaInput): Promise<void> {
-        const credencial = await this.CredencialRepository.buscarPorIdUsuario(idUsuario);
+        const credencial = await this.credencialRepository.buscarPorIdUsuario(idUsuario);
         if (!credencial) throw new NotFoundError(`Credencial para usuario con id ${idUsuario} no encontrada`);
 
         const contrasenaValida = await bcrypt.compare(input.contrasenaActual, credencial.hashContrasena);
         if (!contrasenaValida) throw new UnauthorizedError(`Contraseña actual incorrecta`);
 
         const nuevoHash = await bcrypt.hash(input.nuevaContrasena, 10);
-        await this.CredencialRepository.actualizarContrasena(idUsuario, nuevoHash);
+        await this.credencialRepository.actualizarContrasena(idUsuario, nuevoHash);
     }
 
     // Lógica del caso para cambiar el correo
     async cambiarCorreo(idUsuario: number, input: CambiarCorreoInput): Promise<void> {
-        const credencial = await this.CredencialRepository.buscarPorIdUsuario(idUsuario);
+        const credencial = await this.credencialRepository.buscarPorIdUsuario(idUsuario);
         if (!credencial) throw new NotFoundError(`Credencial para usuario con id ${idUsuario} no encontrada`);
 
         const contrasenaValida = await bcrypt.compare(input.contrasenaActual, credencial.hashContrasena);
         if (!contrasenaValida) throw new UnauthorizedError(`Contraseña actual incorrecta`);
 
+        const correoExiste = await this.credencialRepository.buscarPorCorreo(input.nuevoCorreo);
+        if (correoExiste) throw new ConflictError('El correo ya está registrado');
+
+        await this.credencialRepository.actualizarCorreo(idUsuario, input.nuevoCorreo);
+    }
+
+    // Lógica del caso para resetar la contraseña
+   async resetearContrasena(idUsuario: number, input: ResetContrasenaInput): Promise<void> {
+       const credencial = await this.credencialRepository.buscarPorIdUsuario(idUsuario);
+       if (!credencial) throw new NotFoundError(`Credencial para usuario con id ${idUsuario} no encontrada`);
+
+       const nuevoHash = await bcrypt.hash(input.nuevaContrasena, 10);
+       await this.credencialRepository.actualizarContrasena(idUsuario, nuevoHash);
+   }
 }
