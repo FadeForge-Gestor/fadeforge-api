4084f22718558c90d6cc595f2189fb881c2c4824
Vicente <vicen.dark131@gmail.com>
2026-04-23 11:20:26 -0600

    Se creo el nuevo controlador para los empleados con sus respectivos métodos

--- src/adapters/in/http/empleados/empleados.controller.ts
+++ src/adapters/in/http/empleados/empleados.controller.ts
@@ -19,6 +19,45 @@ export class EmpleadosController {
     }
 
     // Método para listar los empleados que están activos
-    async listarActivos(req: Request, res: Response, next: NextFunction): Promise<void> {}
+    async listarActivos(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const empleados = await this.empleadoUseCase.listarActivos();
+            res.status(200).json(ok(empleados))
+        } catch (error) {
+            next(error);
+        }
+    }
 
+    // Método para obtener un empleado por id
+    async obtenerPorId(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const id = Number(req.params.id);
+            const empleado = await this.empleadoUseCase.obtenerPorId(id);
+            res.status(200).json(ok(empleado));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // Método promover un empleado
+    async promover(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const empleado = await this.empleadoUseCase.promover(req.body);
+            res.status(201).json(ok(empleado));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // Método para desactivar un usuario
+    async desactivar(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const id = Number(req.params.id);
+            await this.empleadoUseCase.desactivar(id);
+            res.status(204).send();
+        } catch (error) {
+            next(error);
+        }
+    }
+    
 }
