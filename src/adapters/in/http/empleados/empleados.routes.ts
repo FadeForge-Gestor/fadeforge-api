85e03c5d2fe75344f032506838da7071ce5531c2
Vicente <vicen.dark131@gmail.com>
2026-04-23 11:33:50 -0600

    Se creo el esquema necesario para promover un empleado

--- src/adapters/in/http/empleados/empleados.routes.ts
+++ src/adapters/in/http/empleados/empleados.routes.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
 import { EmpleadosController } from "./empleados.controller";
 import { EmpleadosUseCase } from "@core/usecases/empleados/empleados.usecase";
+import { EmpleadosPrismaRepository } from "@adapters/out/db/empleados/empleados.prisma.repository";
+import { validate } from "@middlewares/validate.middleware";
+import { authenticate, authorize } from "@middlewares/auth.middleware";
