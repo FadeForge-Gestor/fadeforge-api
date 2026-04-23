d03e0d0fd94516db82560c4d368b9df93768368e
Vicente <vicen.dark131@gmail.com>
2026-04-23 16:08:58 -0600

    Crendo los métodos necesarios para el caso de uso

---
+++ src/core/usecases/categoria-servicio/categoriaServicios.usecase.ts
@@ -0,0 +1,43 @@
+import { ICategoriaServicioRepository } from "@core/ports/out/categoria-servicio/ICategoriaServicioRepository";
+import { ICategoriaServicioUseCase } from "@core/ports/in/categoria-servicio/ICategoriaServicioUseCase";
+import { ConflictError, NotFoundError } from "@shared/errors/HttpError";
+import { CategoriaServicio, CrearCategoriaServicioInput, ActualizarCategoriaServicioInput } from "@core/domain/categoria-servicio/categoriaServicio.entity";
+
+
+export class CategoriasServiciosUseCase implements ICategoriaServicioUseCase {
+
+    constructor(private readonly categoriaServicioRepository: ICategoriaServicioRepository) {}
+
+    // Método para listar las categorias de servicios
+    async listar(): Promise<CategoriaServicio[]> {
+        return this.categoriaServicioRepository.listarTodos();
+    }
+
+    // Método para listar las categorías de servicios activas
+    async listarActivos(): Promise<CategoriaServicio[]> {
+        return this.categoriaServicioRepository.listarActivos();
+    }
+
+    // Método para obtener una categoría de servicios por ID
+    async obtenerPorId(id: number): Promise<CategoriaServicio> {
+        const categoriaServicio = await this.categoriaServicioRepository.buscarPorId(id);
+        if (!categoriaServicio) throw new NotFoundError(`Categoria del servicio con id ${id} no encontrado`);
+        return categoriaServicio;
+    }
+
+    // Método para actualizar un usuario
+    async actualizar(id: number, input: ActualizarCategoriaServicioInput): Promise<CategoriaServicio> {
+        const existe = await this.categoriaServicioRepository.buscarPorId(id);
+        if (!existe) throw new NotFoundError(`Categoria del servicio con id ${id} no encontrado`);
+        return this.categoriaServicioRepository.actualizar(id, input);
+    }
+
+    // Método para desactivar una categoria de servicio
+    async desactivar(id: number): Promise<void> {
+        const existe = await this.categoriaServicioRepository.buscarPorId(id);
+        if (!existe) throw new NotFoundError(`Categoria del servicio con id ${id} no encontrado`);
+        if (!existe.activo) throw new ConflictError(`La categoria del servicio con el id ${id} ya está desactivado`);
+        return this.categoriaServicioRepository.desactivar(id);
+    }
+
+}
