d528999d74e03859239cd9a1c03746b1eb62d853
Vicente <vicen.dark131@gmail.com>
2026-04-28 15:08:39 -0600

    Creamos el caso de uso para crear un nuevo servicio

--- src/core/usecases/servicios/servicios.use.case.ts
+++ src/core/usecases/servicios/servicios.use.case.ts
@@ -4,7 +4,7 @@ import { IServicioUseCase } from "@core/ports/in/servicios/IServicioUseCase";
 import { ConflictError, NotFoundError } from "@shared/errors/HttpError";
 import { Servicio, CrearServicioInput, ActualizarServicioInput } from "@core/domain/servicio/servicio.entity";
 
-export class ServiciosUseCase implements IServicioRepository {
+export class ServiciosUseCase implements IServicioUseCase {
 
     constructor(
         private readonly servicioRepository: IServicioRepository,
@@ -21,11 +21,25 @@ export class ServiciosUseCase implements IServicioRepository {
         return this.servicioRepository.listarActivos();
     }
     
-    // Método para obtener una categoría de servicios por ID
+    // Método para obtener un servicio por ID
     async obtenerPorId(id: number): Promise<Servicio> {
         const categoriaServicio = await this.servicioRepository.buscarPorId(id);
         if (!categoriaServicio) throw new NotFoundError(`Servicio con id ${id} no encontrado`);
         return categoriaServicio;
+    }
+
+    // Método para crear un nuevo servicio
+    async crear(input: CrearServicioInput): Promise<Servicio> {
+
+        // Nos aseguramos que la categoría exista y este activa
+        const categoria = await this.categoriaRepository.buscarPorId(input.idCategoria);
+        if (!categoria) throw new NotFoundError(`Categoría con id ${input.idCategoria} no encontrada`);
+        if (!categoria.activo) throw new ConflictError(`La categoría con id ${input.idCategoria} está desactivada`);
+
+        // Nos aseguramos que el servicio exista
+        const nombreExiste = await this.servicioRepository.buscarPorNombre(input.nombre);
+        if (nombreExiste) throw new ConflictError(`El servicio ${input.nombre} ya existe`);
+        return this.servicioRepository.crear(input);
     }
 
 }
