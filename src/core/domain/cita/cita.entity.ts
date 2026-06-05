c034ad054c1230a51c3c365477d4d7ef8a00c23b
Vicente <vicen.dark131@gmail.com>
2026-06-05 13:37:12 -0600

    Se creo la capa dominio de cita

--- src/core/domain/cita/cita.entity.ts
+++ src/core/domain/cita/cita.entity.ts
@@ -1,15 +1,37 @@
-// Interfaz para la entidad Cita
+type EstadoCita = 'nueva' | 'pendiente' | 'en proceso' | 'finalizada' | 'cancelada' | 'reprogramada' | 'no asistio';
+
 export interface Cita {
     id: number;
     idCliente: number;
     idEmpleado: number;
     fechaInicio: Date;
     fechaFin: Date;
-    estado: 'nueva' | 'pendiente' | 'en proceso' | 'finalizada' | 'cancelada' | 'reprogramada' | 'no asistio';
+    estado: EstadoCita;
     motivoCancelado: string | null;
     fechaCreacion: Date;
     fechaModificacion: Date;
     subtotal: number;
-    iva: number;    
+    iva: number;
     total: number;
+}
+
+export interface CrearCitaInput {
+    idCliente: number;
+    idEmpleado: number;
+    fechaInicio: Date;
+    fechaFin: Date;
+    subtotal: number;
+    iva: number;
+    total: number;
+}
+
+export interface ActualizarCitaInput {
+    idEmpleado?: number;
+    fechaInicio?: Date;
+    fechaFin?: Date;
+    estado?: EstadoCita;
+    motivoCancelado?: string;
+    subtotal?: number;
+    iva?: number;
+    total?: number;
 }
