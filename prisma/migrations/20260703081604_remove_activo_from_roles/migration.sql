fee72bbd7163e20d150e8267fdd9e82f4ec50d9e
Vicente <vicen.dark131@gmail.com>
2026-07-03 02:17:19 -0600

    chore(prisma): agregar migración remove_activo_from_roles
    Migración generada y aplicada con prisma migrate dev: dropea la
    columna activo de la tabla roles en el schema seguridad.

---
+++ prisma/migrations/20260703081604_remove_activo_from_roles/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `activo` on the `roles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "seguridad"."roles" DROP COLUMN "activo";
